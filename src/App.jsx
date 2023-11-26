982b823cfe3b3d0c923f9e9a12c342451068742f
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-11-26 12:20:36 +0530

    Setting up states in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
+import InputBox from './components/InputBox'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
 
   return (
     <>
